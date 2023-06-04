721836338cda8410f9748361a476c5f4f770f97e
Михаил <colistop@mail.ru>
2023-06-04 17:37:30 +1000

    Добавьте файлы проекта.

---
+++ calculator/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Index(Calculator calculator)
+        {
+
+            if (calculator.calculate == "*")
+            {
+                calculator.result = calculator.value1 * calculator.value2;
+            }
+            else
+            {
+                calculator.result = calculator.value1 + calculator.value2;
+            }
+           
+
+            ViewData["result"] = calculator.result;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ calculator/Models/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace calculator.Models
+{
+    public class Calculator
+    {
+        public int value1 { get; set; }
+        public int value2 { get; set; }
+        public string calculate { get; set; }
+        public int result { get; set; }
+    }
+}
